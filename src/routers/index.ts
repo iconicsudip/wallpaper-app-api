77c05671e460335e249731fe2997cf239382beff
Sudip Das <das366966@gmail.com>
2024-01-01 20:07:03 +0530

    first commit

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,9 +3,9 @@ import wallpaperRouters from "./wallpaper.router";
 import categoryRouters from "./category.router";
 
 const mainRoutes = Router();
-mainRoutes.get("/",(req,res)=>{
-    res.send("Api is working");
-})
+// mainRoutes.get("/",(req,res)=>{
+//     res.send("Api is working");
+// })
 mainRoutes.use("/api/v1/wallpapers",wallpaperRouters)
 mainRoutes.use("/api/v1/categories",categoryRouters)
 
